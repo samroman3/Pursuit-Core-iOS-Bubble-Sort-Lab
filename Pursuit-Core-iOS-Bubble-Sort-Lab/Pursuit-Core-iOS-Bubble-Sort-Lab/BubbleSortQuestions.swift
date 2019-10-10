c3a337f8b7dfcfbe25ed4513208396b7c9814a4a
Sam <samroman3@gmail.com>
2019-10-10 09:41:28 -0400

    First two tests successful

--- Pursuit-Core-iOS-Bubble-Sort-Lab/Pursuit-Core-iOS-Bubble-Sort-Lab/BubbleSortQuestions.swift
+++ Pursuit-Core-iOS-Bubble-Sort-Lab/Pursuit-Core-iOS-Bubble-Sort-Lab/BubbleSortQuestions.swift
@@ -5,6 +5,17 @@ import Foundation
 // Implement bubble sort that accepts a closure about how to sort elements
 
 func bubbleSort<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
+    var arr = arr
+    var madeSwap = false
+    repeat {
+        madeSwap = false
+        for i in 0..<arr.count - 1 {
+            if !isSorted(arr[i], arr[i + 1]) && arr[i] != arr[i+1] {
+                arr.swapAt(i, i + 1)
+                madeSwap = true
+            }
+        }
+    } while madeSwap
     return arr
 }
 
@@ -14,7 +25,33 @@ func bubbleSort<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
 // https://www.geeksforgeeks.org/sort-an-array-without-changing-position-of-negative-numbers/
 
 func bubbleSortWithoutMovingNegatives<T: Comparable>(arr: [T], by isSorted: (T, T) -> Bool) -> [T] {
-    return arr
+   let arr = arr
+   if arr.isEmpty {
+       return [T]()
+   }
+   var madeSwap = false
+   let zero = 0 as! T
+   var positiveArr = arr.filter { $0 >= zero }
+   var answer = [T]()
+   var positiveIndex = 0
+   repeat {
+       madeSwap = false
+       for i in 0..<positiveArr.count - 1 {
+           if !isSorted(positiveArr[i], positiveArr[i+1]) && positiveArr[i] != positiveArr[i+1] {
+               positiveArr.swapAt(i, i+1)
+               madeSwap = true
+           }
+       }
+   } while madeSwap
+   for i in arr {
+       if i < zero {
+           answer.append(i)
+       } else {
+           answer.append(positiveArr[positiveIndex])
+           positiveIndex += 1
+       }
+   }
+   return answer
 }
 
 // Question Three
